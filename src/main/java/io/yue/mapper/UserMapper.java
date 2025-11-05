9cccfda2513a2e3cbffd459f03efef3af47fb2dc
siehun <126447514+siehun@users.noreply.github.com>
2025-11-05 16:12:36 +0800

    fix: add UserMapper.java

---
+++ src/main/java/io/yue/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package io.yue.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.yue.pojo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author siehun
+*/
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
